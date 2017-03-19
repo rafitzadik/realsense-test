15e744ec240bf20fdda5d9000af0d1b6ce054fa8
rafitzadik <rafitzadik@gmail.com>
2017-03-19 11:19:24 -0700

    more games with averaging over history and smoothing out noise - much better now

--- hand_play.py
+++ hand_play.py
@@ -8,43 +8,33 @@ import numpy as np
 import cv2
 import pyrealsense as pyrs
 from scipy.stats import threshold
+from scipy.ndimage.filters import gaussian_filter
+from scipy.ndimage.morphology import grey_dilation, grey_erosion
 
-thresh_lo = 500 # that's actually the physics of the R200 - it can detect from 50cm 
-thresh_hi = 1500 #ignore anything beyond 1m for now
-min_contour = 5000 #area of smallest contour that we care about
+thresh_lo = 700 # that's actually the physics of the R200 - it can detect from 50cm 
+thresh_hi = 1200 #ignore anything beyond 1m for now
+min_contour = 100 #area of smallest contour that we care about
 min_palm_circle = 10
+max_depth_hist = 10
 
-def avg_depth(depth, pt):
-#find the depth at pont (x,y) by averaging out the meaningful values around (x,y)
-    max_hist = 10
-    try:
-        if (len(avg_depth.hist) > max_hist):
-            avg_depth.hist = avg_depth.hist[:-1]
-    except AttributeError:
-        avg_depth.hist = []
-    avg_depth.hist.insert(0, depth)
+def push_depth_hist(depth_hist, d):
+    if (len(depth_hist) > max_depth_hist):
+        depth_hist = depth_hist[:-1]
+           
+    #first remove small areas of zeros
+    d = grey_dilation(d, size=(5,5)) 
+    #and add to history
+    depth_hist.insert(0, d)
+    return depth_hist            
+
+def smooth_depth(depth_hist):
+    #now give the history average for each pixel:
+    #return depth_hist[0]
+    smooth = sum(depth_hist) / len(depth_hist)
+    return smooth
     
-    [x,y] = pt 
-    x = int(x)
-    y = int(y)
-    if (x < 2): x = 2
-    if (x > len(depth[0]) - 2): x = len(depth[0]-2)
-    if (y < 2): y = 2
-    if (y > len(depth) - 2): y = len(depth)-2
-    #return depth[y,x]
-        
-    d = 0
-    n = 0
-    for h in avg_depth.hist:
-        for i in range(y-2, y+3):
-            for j in range(x-2, x+3):
-                if depth[i,j] > 10:
-                    n += 1
-                    d += h[i,j]
-    if n > 0:            
-        return np.array([(d / n)])
-    else:
-        return np.array([0])
+def get_depth(d, pixel):
+    return d[pixel[1],pixel[0]]
     
 #nice idea, but couldn't get this to work against the backdrop of a wall that has the same hue as my hand...
 def filter_hsv(color, depth, hand, palm_center, palm_radius):
@@ -92,25 +82,23 @@ def my_canny(image, sigma=0.7):
     return edged
 
 def detect_hand(color, depth):
-    pre_thresh = depth
-    thresh = threshold(pre_thresh, threshmin = thresh_lo, threshmax = thresh_hi, newval = 0) #throw out all values that are too small or too large
+    thresh = threshold(depth, threshmin = thresh_lo, threshmax = thresh_hi, newval = 0) #throw out all values that are too small or too large
     thresh = threshold(thresh, threshmax = 1, newval = 255) #make remaining values 255
     thresh = thresh.astype(np.uint8)    
-    #now dilate the image: make more things white around white areas
-    kernel = np.ones((10,10), np.uint8)
+    kernel = np.ones((5,5), np.uint8)
+    thresh = cv2.erode(thresh, kernel, iterations=1)
+    kernel = np.ones((7,7), np.uint8)
     thresh_dilation = cv2.dilate(thresh, kernel, iterations=1)
-    #now find the contours
+
     _, contours, _ = cv2.findContours(thresh_dilation,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
     rois = [] #regions of interest. each one is a tuple: (contour, area, center)
     #print
+    print 'len(contours): ', len(contours)
     for contour in contours:
         r = cv2.minAreaRect(contour)
         (r_cent, (w,h), r_angle) = r
-        if (cv2.contourArea(contour) > min_contour and #enough area
-            (w / h > 1.5 or h / w > 1.5) and          #tall or long rect, not a square
-            (min([x for [[x,y]] in contour]) > 50)):      #not at the left most part
-                #print h, w, h/w, r_angle
+        if cv2.contourArea(contour) > min_contour and min([x for [[x,y]] in contour]) > 50:
                 M = cv2.moments(contour)
                 rois.append( (contour, M['m00'], (int(M['m10']/M['m00']), int(M['m01']/M['m00']))))
 
@@ -132,6 +120,7 @@ def detect_hand(color, depth):
             palm_cent = (int(x),int(y))
             palm_radius = radius
         if (palm_cent != None and radius < 0.9 * palm_radius):
+            #hand = filter_hsv(color, depth, hand, palm_cent, palm_radius)    
             break #we reached a local min
     return hand, palm_cent, palm_radius
     
@@ -144,11 +133,11 @@ def color_to_canny(pic):
 def palm_vectors(dev, d, palm_cent):
     p2 = np.array([palm_cent[0] + 10, palm_cent[1]], np.uint)
     #d2 = np.array([d[p2[1],p2[0]]])
-    d2 = avg_depth(d, p2)
+    d2 = get_depth(d, p2)
     p2_3d = dev.deproject_pixel_to_point(p2, d2)
     p3 = np.array([palm_cent[0] , palm_cent[1] + 10], np.uint)
     #d3 = np.array([d[p3[1],p3[0]]])
-    d3 = avg_depth(d, p3)
+    d3 = get_depth(d, p3)
     p3_3d = dev.deproject_pixel_to_point(p3, d3)
     p2_3d_d = p2_3d - palm_cent_3d
     p2_3d_n = p2_3d_d / np.linalg.norm(p2_3d_d) 
@@ -182,23 +171,6 @@ def draw_hand(color, depth, hand, palm_cent, palm_diameter, pt, pixel2, pixel3, 
         if not np.isnan(pixel4[0]):
             cv2.line(pic, palm_cent, pixel4, (0,255,0))
     return pic
-
-#def update_ball(ball_pos, ball_v, dt, palm_cent, palm_center_depth):
-#    ball_pos += [a * dt for a in ball_v]
-#    if (ball_pos[2] < 10):
-#        ball_v[0] = random()
-#        ball_v[1] = random()
-#        ball_v[2] = 10
-#    elif (ball_pos[2] > 3000):
-#        ball_v[0] = random()
-#        ball_v[1] = random()
-#        ball_v[2] = -10       
-#    elif (hits(ball_pos, palm_center, palm_center_depth) || ball_pos[2] > 3000):
-#        ball_v = []
-    
-#    return ball_pos, ball_v
-    
-#def draw_ball(ball_pos, pic):
     
 def smooth(c, r, hist):
     max_hist = 10
@@ -229,7 +201,7 @@ def smooth(c, r, hist):
 if __name__ == '__main__': 
     pyrs.start()
     dev = pyrs.Device()
-
+    
     #Use appropriate settings here to get the exposure you need
     dev.set_device_option(pyrs.constants.rs_option.RS_OPTION_COLOR_ENABLE_AUTO_EXPOSURE, 1)
     dev.set_device_option(pyrs.constants.rs_option.RS_OPTION_R200_LR_AUTO_EXPOSURE_ENABLED, 1)
@@ -241,6 +213,7 @@ if __name__ == '__main__':
     smoothing = 0.9;
     fps_smooth = 30
     hist = []
+    depth_hist = []
 #    outFile = None
 
     while True:
@@ -257,12 +230,14 @@ if __name__ == '__main__':
         c = dev.colour
         c = cv2.cvtColor(c, cv2.COLOR_RGB2BGR)
         d = dev.dac * dev.depth_scale * 1000 #this makes d be the depth in mm
+        depth_hist = push_depth_hist(depth_hist, d)
+        d = smooth_depth(depth_hist)
         hand, palm_cent, palm_radius = detect_hand(c,d)
         palm_cent_smooth, palm_radius_smooth, hist = smooth(palm_cent, palm_radius, hist)
         if (palm_cent_smooth != None):
             pixel = np.array([palm_cent_smooth[0], palm_cent_smooth[1]], np.uint)
             #depth = np.array([d[pixel[1],pixel[0]]])
-            depth = avg_depth(d, pixel)
+            depth = get_depth(d, pixel)
             palm_cent_3d = dev.deproject_pixel_to_point(pixel, depth)
             p2_map, p3_map, p4_map = palm_vectors(dev, d, palm_cent_smooth)
         else:
@@ -272,10 +247,12 @@ if __name__ == '__main__':
             pic = draw_hand(c,d,hand, palm_cent_smooth, palm_radius_smooth, palm_cent_3d, p2_map, p3_map, p4_map)
         else:
             pic = np.zeros(c.shape, np.uint8)
-    
+        
+        #pic = cv2.applyColorMap(d.astype(np.uint8), cv2.COLORMAP_RAINBOW)    
         cd = np.concatenate((c,pic), axis=1)
     
-        cv2.putText(cd, str(fps_smooth)[:4], (0,50), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255))
+        #cv2.putText(cd, str(fps_smooth)[:4], (0,50), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255))
+        cv2.putText(cd, str(d[240, 320])[:4], (0,50), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255))
     
         cv2.imshow('', cd)
     #    if (outFile == None):
